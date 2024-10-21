0ff3c3c2c03c1d85aab7695f4ff5f40e9e3f1457
Makadur4 <rm90349@estudante.fieb.edu.br>
2024-10-21 20:35:35 -0300

    atualizando

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,26 +1,63 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function Login(props) {
-  function Logar() {
-    props.setStatue(true);
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
+
+  function Logar(e) {
+    e.preventDefault();
+
+    const params = {
+      login: login,
+      senha: senha,
+    };
+
+    axios
+      .get("http://localhost:8080/usuario", { params })
+      .then(function (response) {
+        props.setStatue(true);
+      })
+      .catch(function (erro) {
+        alert("Usuário não localizado!");
+        props.setStatue(false);
+      });
   }
 
   return (
     <main className="login">
-      <div>
-        <div className="foto_perfil">
-          <img src="/svg/icone_perfil_azul.svg" />
+      <form onSubmit={Logar}>
+        <div>
+          <div className="foto_perfil">
+            <img src="/svg/icone_perfil_azul.svg" />
+          </div>
+          <label className="label" htmlFor="login">
+            Login:
+          </label>
+          <input
+            type="text"
+            className="input"
+            id="login"
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+            required={true}
+          ></input>
+          <label className="label" htmlFor="senha">
+            Senha:
+          </label>
+          <input
+            type="password"
+            className="input2"
+            id="senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            required={true}
+          ></input>
         </div>
-        <label className="label" htmlFor="email">
-          E-mail:
-        </label>
-        <input type="text" className="input" id="email"></input>
-        <label className="label" htmlFor="senha">
-          Senha:
-        </label>
-        <input type="text" className="input2" id="senha"></input>
-      </div>
-      <div className="botao_continuar">
-        <button onClick={Logar}>Continuar</button>
-      </div>
+        <div className="botao_continuar">
+          <button type="submit">Continuar</button>
+        </div>
+      </form>
     </main>
   );
 }
